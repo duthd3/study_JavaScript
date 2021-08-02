3f5d42b7862ce6ff768b8c510fddc3da118cb148
YeoSong <duthd3@naver.com>
2021-08-02 23:58:39 +0900

    Create 7.8.js

---
+++ ch07/7.8.js
@@ -0,0 +1,16 @@
+typeof ''; //'string'
+typeof 1; //'number'
+typeof NaN; //'number'
+typeof true; //'boolean'
+typeof undefined; //'undefined'
+typeof Symbol(); //'symbol'
+typeof null; //'object'
+typeof []; //'object'
+typeof {}; //'object'
+typeof new Date(); //'object'
+typeof /test/gi; //'object'
+typeof function(){}; //'function'
+
+var foo = null;
+typeof foo === null;
+foo === null;
