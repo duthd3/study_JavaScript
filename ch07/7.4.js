ab0bfcae0deca81ada233d64d3e788d998f243e2
YeoSong <duthd3@naver.com>
2021-08-02 23:58:03 +0900

    Create 7.4.js

---
+++ ch07/7.4.js
@@ -0,0 +1,10 @@
+var x = 2;
+
+//2 %2는 0이고 0은 false로 암묵적 타입 변환된다.
+var result = x%2 ? '홀수' : '짝수';
+console.log(result);
+
+var x=2, result;
+if(x%2) result = '홀수';
+else result = '짝수';
+console.log(result);
