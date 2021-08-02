8e416a7ba4f1134337c762282329ec65271d161d
YeoSong <duthd3@naver.com>
2021-08-02 23:58:12 +0900

    Create 7.5.js

---
+++ ch07/7.5.js
@@ -0,0 +1,22 @@
+//논리합(||) 연산자
+true || true ; //true
+true || false; //true
+false || true; //true
+false || false; //false
+
+//논리곱(&&) 연산자
+true && true; //true
+true && false; //false
+false && true; //false
+false && false; //false
+
+//논리 부정(!) 연산자
+!true; //false
+!false; //true
+
+//암묵적 타입 변환
+!0; //true
+!'Hello' //false
+
+//단축 평가
+'Cat' && 'Dog';// 'Dog'
