eb36e98910c787cedefb0bfb07e374b034826e82
YeoSong <duthd3@naver.com>
2021-08-07 16:34:42 +0900

    Create 08-10.js

---
+++ ch08/08-10.js
@@ -0,0 +1,29 @@
+for(var i=0; i<2; i++){
+    console.log(i);
+}
+
+for(var i=1; i>=0; i--){
+    console.log(i);
+    
+}
+var count = 0;
+
+while(count < 3){
+    console.log(count);
+    count++;
+}
+
+var count = 0;
+
+while(true){
+    console.log(count);
+    count++;
+    if(count === 3 ) break;
+}
+
+var count = 0;
+
+do{
+    console.log(count);
+    count++;
+}while(count<3);
