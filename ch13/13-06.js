ed0c7e4d6bdec57469cbbf203f4fb2864137accb
YeoSong <duthd3@naver.com>
2021-08-25 23:44:22 +0900

    Create 13-06.js

---
+++ ch13/13-06.js
@@ -0,0 +1,14 @@
+//전역함수
+function foo(){
+    console.log('global function foo');
+}
+
+function bar(){
+    //중첩 함수
+    function foo(){
+        console.log('local function foo');
+    }
+    foo();
+}
+
+bar();
